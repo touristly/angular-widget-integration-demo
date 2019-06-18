7240ae45f20653b7c0f8172f32cc1defd51d1577
Manish Singh <manishkumar.singh103@gmail.com>
2019-06-18 11:10:58 +0530

    angular 7 project setup and activity widget component use

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+﻿import { NgModule, CUSTOM_ELEMENTS_SCHEMA }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+
+import { HomeComponent } from './home';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+
+export class AppModule { }
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+﻿import './polyfills';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+import { defineCustomElements } from 'activity-search-widget/dist/loader';
+
+platformBrowserDynamic()
+	.bootstrapModule(AppModule)
+	.catch(err => console.log(err));;
+
+defineCustomElements(window);
